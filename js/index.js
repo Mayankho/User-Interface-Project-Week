21bb3e15ffb502d98062e1a7a870f46bb23da539
Mayankho <yankhot96@gmail.com>
2019-02-14 23:53:56 -0500

    mayankho-trumble

--- js/index.js
+++ js/index.js
@@ -1,1 +1,12 @@
 // JS goes here
+
+//Navigation
+
+const nav = document.querySelector(".nav");
+const headerTag = document.querySelector("header");
+const headerBox = document.querySelector(".header");
+const hamburger = document.querySelector("hamburger");
+const container = docuent.querySelector(".container");
+const headerDiv = document.querySelector(".header-div");
+
+
