82ec2ece48a9235020ab3e47268a70f8001713eb
Mayankho <yankhot96@gmail.com>
2019-02-15 22:32:14 -0500

    Added nav functionality

--- js/index.js
+++ js/index.js
@@ -9,4 +9,24 @@ const hamburger = document.querySelector("hamburger");
 const container = docuent.querySelector(".container");
 const headerDiv = document.querySelector(".header-div");
 
+let counter = 2; // set variable named counter
+hamburger.addEventListener("click", () => { // setting event listener to the hamburgr symbol
+  if (counter % 2 === 0) {
+    nav.classList.remove("nav-ghost");
+    headerBox.classList.add("expanded");
+    nav.classList.add("expanded-nav");
+    headerTag.classList.add("expanded-header");
+    headerDiv.classList.add("expanded-header-div");
+    hamburger.src = "img\\nav-hamburger-close.png";
+  } else {
+    nav.classList.add("nav-hidden");
+    headerBox.classList.remove("expanded");
+    nav.classList.remove("expanded-nav");
+    headerTag.classList.remove("expanded-header");
+    headerDiv.classList.remove("expanded-header-div");
+    hamburger.src = "img\\nav-hamburger.png";
+  }
+  counter++;
+});
+
 
