fdf92a3af864bf41f2559734779d4691131a1295
Mayankho <yankhot96@gmail.com>
2019-02-19 21:48:48 -0500

    All done except tab js

--- js/index.js
+++ js/index.js
@@ -5,8 +5,8 @@
 const nav = document.querySelector(".nav");
 const headerTag = document.querySelector("header");
 const headerBox = document.querySelector(".header");
-const hamburger = document.querySelector("hamburger");
-const container = docuent.querySelector(".container");
+const hamburger = document.querySelector("#hamburger");
+const container = document.querySelector(".container");
 const headerDiv = document.querySelector(".header-div");
 
 let counter = 2; // set variable named counter
@@ -19,7 +19,7 @@ hamburger.addEventListener("click", () => { // setting event listener to the ham
     headerDiv.classList.add("expanded-header-div");
     hamburger.src = "img\\nav-hamburger-close.png";
   } else {
-    nav.classList.add("nav-hidden");
+    nav.classList.add("nav-ghost");
     headerBox.classList.remove("expanded");
     nav.classList.remove("expanded-nav");
     headerTag.classList.remove("expanded-header");
